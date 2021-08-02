0bafbb0d8918afc07a52b40cf93a77b288625ee6
samuel addisu <samueladdisu7@gmail.com>
2021-08-02 17:14:10 +0300

    home page

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 const menu = document.querySelector('.menu')
 const linksContainer = document.querySelector('.links-container')
+const dropdown = document.querySelector('.drop-down')
+const dropdownContent = document.querySelector('.drop-down-link')
 
 
 menu.addEventListener('click',() => {
     linksContainer.classList.toggle('show-links')
+})
+dropdown.addEventListener('mouseenter', () => {
+    
+    dropdownContent.classList.add('display-links')
+})
+dropdown.addEventListener('mouseleave', () => {
+
+    dropdownContent.classList.remove('display-links')
 })
