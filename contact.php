4c09ef4a9ac870e1025996bac7800010f22048d1
samuel addisu <samueladdisu7@gmail.com>
2021-08-16 15:37:49 +0300

    site verify

--- contact.php
+++ contact.php
@@ -1,6 +1,15 @@
 <?php
-    echo $_POST["submit"];
     if(isset($_POST['submit'])){
         echo 'form submited';
-     
+        echo 'form submitted';
+        $to = 'sales@bewelltradingco.com';
+        $header = $_POST['email'];
+        $subject = $_POST['subject'];
+        $msg = $_POST['msg'];
+
+        if ( mail($to, $subject, $msg, $header)) {
+            header('Location: ./contact.html');
+         } else {
+            echo("Email sending failed...");
+         }
     }
