6a129ad8d87b16bf7211a3c2394d3bf3dc4776cf
samuel addisu <samueladdisu7@gmail.com>
2021-08-16 17:22:48 +0300

    other method to fix email

--- contact.php
+++ contact.php
@@ -1,13 +1,15 @@
 <?php
     if(isset($_POST['submit'])){
-        echo 'form submited';
-        echo 'form submitted';
-        $to = 'samuel@versavvymedia.com';
-        $header = $_POST['email'];
-        $subject = $_POST['subject'];
-        $msg = $_POST['msg'];
+        
+        $to      = 'sales@versavvymedia.com'; 
+        $subject = $_POST['subject']; 
+        $message = $_POST['msg']; 
+        $headers = 'From: '. $_POST['email'] . "\r\n" .  
+
+    'X-Mailer: PHP/' . phpversion(); 
 
         if ( mail($to, $subject, $msg, $header)) {
+            echo "<script> alert('Thanks For Contacting Us We will Respond to you shortly'); </script>";
             header('Location: ./contact.html');
          } else {
             echo("Email sending failed...");
--- js/about.js
+++ js/about.js
@@ -12,7 +12,6 @@ const storage = [
         text: `Established in 2020, our goal is to provide every client personable and professional service. We understand the value in delivering creative technologies, implementation, and timely support, in order to make a quantifiable impact on our clients business. Our partners rely on us to  bridge the gap between those who produce technology and those who use it.
         ` ,
         img: `<img src="./assets/storage/4.jpg" alt="">`,
-        bgimg: `<img src="./assets/about/v.png" class="v" alt="">`
     }
 ]
 
