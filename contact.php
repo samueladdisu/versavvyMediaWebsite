c1dd6bf7ef4e9b88dc07706efa762e99c7c4e00f
samuel addisu <samueladdisu7@gmail.com>
2021-08-16 15:36:56 +0300

    site verification

--- contact.php
+++ contact.php
@@ -1,21 +1,6 @@
 <?php
     echo $_POST["submit"];
     if(isset($_POST['submit'])){
-
-        echo 'form submitted';
-        // $name = $_POST['fname'];
-        $to = 'samuel@versavvymedia.com';
-        $header =  'From: ' . $_POST['email'] . "\r\n" . 
-        
-            'Reply-To: test@test.com' . "\r\n" . 
-        
-            'X-Mailer: PHP/' . phpversion();
-        $subject = $_POST['subject'];
-        $msg = $_POST['msg'];
-
-        if ( mail($to, $subject, $msg, $header)) {
-            header('Location: ./contact.html');
-         } else {
-            echo("Email sending failed...");
-         }
+        echo 'form submited';
+     
     }
