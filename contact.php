f2035df2400a98d821dd659e3176ac723775aa85
samuel addisu <samueladdisu7@gmail.com>
2021-08-16 17:25:11 +0300

    silly mistake cost me whole day

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
 
     'X-Mailer: PHP/' . phpversion(); 
 
-        if ( mail($to, $subject, $msg, $header)) {
+        if ( mail($to, $subject, $message, $header)) {
             echo "<script> alert('Thanks For Contacting Us We will Respond to you shortly'); </script>";
             header('Location: ./contact.html');
          } else {
