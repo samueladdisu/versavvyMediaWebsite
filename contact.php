74064e02705ba5ab7536970ad6fbc7fcd33efe8c
samuel addisu <samueladdisu7@gmail.com>
2021-08-17 10:53:45 +0300

    php mailer

--- contact.php
+++ contact.php
@@ -1,17 +1,48 @@
 <?php
+   
+   use PHPMailer\PHPMailer\PHPMailer;
+   use PHPMailer\PHPMailer\Exception;
     if(isset($_POST['submit'])){
+     
         
-        $to      = 'sales@versavvymedia.com'; 
-        $subject = $_POST['subject']; 
-        $message = $_POST['msg']; 
-        $headers = 'From: '. $_POST['email'] . "\r\n" .  
+        require '/home/cpanelusername/PHPMailerTest/PHPMailer/src/Exception.php';
+        require '/home/cpanelusername/PHPMailerTest/PHPMailer/src/PHPMailer.php';
+        require '/home/cpanelusername/PHPMailerTest/PHPMailer/src/SMTP.php';
+        
+        // Instantiation and passing [ICODE]true[/ICODE] enables exceptions
+        $mail = new PHPMailer(true);
+        
+        try {
+         //Server settings
+         $mail->SMTPDebug = 2; // Enable verbose debug output
+         $mail->isSMTP(); // Set mailer to use SMTP
+         $mail->Host = 'versavvymedia.com'; // Specify main and backup SMTP servers
+         $mail->SMTPAuth = true; // Enable SMTP authentication
+         $mail->Username = 'user@example.com'; // SMTP username
+         $mail->Password = 'secret'; // SMTP password
+         $mail->SMTPSecure = 'tls'; // Enable TLS encryption, [ICODE]ssl[/ICODE] also accepted
+         $mail->Port = 587; // TCP port to connect to
+        
+        //Recipients
+         $mail->setFrom('from@example.com', 'Mailer');
+         $mail->addAddress('sales@versavvymedia.com', 'Joe User'); // Add a recipient
+         $mail->addAddress('recipient2@example.com'); // Name is optional
+         $mail->addReplyTo('info@example.com', 'Information');
+         $mail->addCC('cc@example.com');
+         $mail->addBCC('bcc@example.com');
+        
 
-    'X-Mailer: PHP/' . phpversion(); 
-
-        if ( mail($to, $subject, $message, $headers)) {
-            echo "<script> alert('Thanks For Contacting Us We will Respond to you shortly'); </script>";
-            header('Location: ./contact.html');
-         } else {
-            echo("Email sending failed...");
-         }
+        
+        // Content
+         $mail->isHTML(true); // Set email format to HTML
+         $mail->Subject = $_POST["subject"];
+         $mail->Body = $_POST['msg'];
+         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        
+        $mail->send();
+         echo 'Message has been sent';
+        
+        } catch (Exception $e) {
+         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
     }
