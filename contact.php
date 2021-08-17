6926ea3ef551bd868c89b18e086b7fc7ef3e41c6
samuel addisu <samueladdisu7@gmail.com>
2021-08-17 13:25:17 +0300

    email

--- contact.php
+++ contact.php
@@ -1,48 +1,20 @@
 <?php
-   
-   use PHPMailer\PHPMailer\PHPMailer;
-   use PHPMailer\PHPMailer\Exception;
+
     if(isset($_POST['submit'])){
      
-        
-        require '/home/cpanelusername/PHPMailerTest/PHPMailer/src/Exception.php';
-        require '/home/cpanelusername/PHPMailerTest/PHPMailer/src/PHPMailer.php';
-        require '/home/cpanelusername/PHPMailerTest/PHPMailer/src/SMTP.php';
-        
-        // Instantiation and passing [ICODE]true[/ICODE] enables exceptions
-        $mail = new PHPMailer(true);
-        
-        try {
-         //Server settings
-         $mail->SMTPDebug = 2; // Enable verbose debug output
-         $mail->isSMTP(); // Set mailer to use SMTP
-         $mail->Host = 'versavvymedia.com'; // Specify main and backup SMTP servers
-         $mail->SMTPAuth = true; // Enable SMTP authentication
-         $mail->Username = 'user@example.com'; // SMTP username
-         $mail->Password = 'secret'; // SMTP password
-         $mail->SMTPSecure = 'tls'; // Enable TLS encryption, [ICODE]ssl[/ICODE] also accepted
-         $mail->Port = 587; // TCP port to connect to
-        
-        //Recipients
-         $mail->setFrom('from@example.com', 'Mailer');
-         $mail->addAddress('sales@versavvymedia.com', 'Joe User'); // Add a recipient
-         $mail->addAddress('recipient2@example.com'); // Name is optional
-         $mail->addReplyTo('info@example.com', 'Information');
-         $mail->addCC('cc@example.com');
-         $mail->addBCC('bcc@example.com');
-        
+      
+        echo 'form submitted';
+        // $name = $_POST['fname'];
+        $to = 'samueladdisu7@gmail.com';
+        $header = $_POST['email'];
+        $subject = $_POST['subject'];
+        $msg = $_POST['msg'];
 
-        
-        // Content
-         $mail->isHTML(true); // Set email format to HTML
-         $mail->Subject = $_POST["subject"];
-         $mail->Body = $_POST['msg'];
-         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-        
-        $mail->send();
-         echo 'Message has been sent';
-        
-        } catch (Exception $e) {
-         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        }
+        if ( mail($to, $subject, $msg, $header)) {
+            echo "<script> alert('message sent sucssesfully'); </script>";
+            header('Location: ./contact.html');
+         } else {
+            echo("Email sending failed...");
+            echo "<script> alert('message not sent'); </script>";
+         }
     }
