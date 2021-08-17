6843e6319d043beb17992d7bc15be3f7fc8b6b51
samuel addisu <samueladdisu7@gmail.com>
2021-08-17 13:52:25 +0300

    gfg mailer

--- contact.php
+++ contact.php
@@ -1,20 +1,34 @@
 <?php
-
-    if(isset($_POST['submit'])){
-     
-      
-        echo 'form submitted';
-        // $name = $_POST['fname'];
-        $to = 'samueladdisu7@gmail.com';
-        $header = $_POST['email'];
-        $subject = $_POST['subject'];
-        $msg = $_POST['msg'];
-
-        if ( mail($to, $subject, $msg, $header)) {
-            echo "<script> alert('message sent sucssesfully'); </script>";
-            header('Location: ./contact.html');
-         } else {
-            echo("Email sending failed...");
-            echo "<script> alert('message not sent'); </script>";
-         }
-    }
+  
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+  
+require 'vendor/autoload.php';
+  
+$mail = new PHPMailer(true);
+  
+try {
+    $mail->SMTPDebug = 2;                                       
+    $mail->isSMTP();                                            
+    $mail->Host       = 'smtp.versavvymedia.com;';                    
+    $mail->SMTPAuth   = true;                             
+    $mail->Username   = 'samuel@versavvymedia.com';                 
+    $mail->Password   = '$@mu3l@202!';                        
+    $mail->SMTPSecure = 'tls';                              
+    $mail->Port       = 587;  
+  
+    $mail->setFrom('samuel@versavvymedia.com', 'Name');           
+    $mail->addAddress('sales@versavvymedia.com');
+    // $mail->addAddress('receiver2@gfg.com', 'Name');
+       
+    $mail->isHTML(true);                                  
+    $mail->Subject = $_POST['subject'];
+    $mail->Body    = $_POST['msg'];
+    $mail->AltBody = $_POST['msg'];
+    $mail->send();
+    echo "Mail has been sent successfully!";
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+  
+?>
